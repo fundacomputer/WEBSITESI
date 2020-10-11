69547964b81261431a93bc8f59b0b92745470336
fundacomputer <72304416+fundacomputer@users.noreply.github.com>
2020-10-11 18:28:35 +0300

    Initial commit

---
+++ KOCAK BAKLAVA/proje.js
@@ -0,0 +1,146 @@
+
+/*function over(){
+    $('#test').attr("src","img/bulbulyuvasi1.jpg");
+}
+function out(){
+    
+    $('#test').attr("src","img/bulbulyuvasi.jpg")
+}*/
+
+
+$(document).ready(function() { 
+    var mEnterIMG = "img/bulbulyuvasi.jpg"; 
+    var mLeaveIMG = "img/bulbulyuvasi1.jpg"; 
+     
+        $( "#test" ).hover(function(){ 
+            $(this).attr("src", mEnterIMG); 
+        }, function(){ 
+            $(this).attr("src", mLeaveIMG); 
+        }); 
+    }); 
+
+  $(document).ready(function() { 
+        var mEnterIMG = "img/lead.jpg"; 
+        var mLeaveIMG = "img/lead1.jpg"; 
+         
+            $( "#test1" ).hover(function(){ 
+                $(this).attr("src", mEnterIMG); 
+            }, function(){ 
+                $(this).attr("src", mLeaveIMG); 
+            }); 
+        }); 
+
+
+ $(document).ready(function() { 
+            var mEnterIMG = "img/full.jpg"; 
+            var mLeaveIMG = "img/full1.jpg"; 
+             
+                $( "#test2" ).hover(function(){ 
+                    $(this).attr("src", mEnterIMG); 
+                }, function(){ 
+                    $(this).attr("src", mLeaveIMG); 
+                }); 
+            }); 
+
+$(document).ready(function() { 
+     var mEnterIMG = "img/söbiyet.jpg";
+      var mLeaveIMG = "img/söbiyet1.jpg"; 
+                 
+         $( "#test3" ).hover(function(){ 
+          $(this).attr("src", mEnterIMG); 
+             }, function(){ 
+           $(this).attr("src", mLeaveIMG); 
+                    }); 
+                }); 
+
+
+   $(document).ready(function() { 
+      var mEnterIMG = "img/havucdilimi.jpg"; 
+    var mLeaveIMG = "img/havucdilimi1.jpg"; 
+      $( "#test4" ).hover(function(){ 
+      $(this).attr("src", mEnterIMG); 
+          }, function(){ 
+    $(this).attr("src", mLeaveIMG); 
+                        }); 
+                    }); 
+
+
+     $(document).ready(function() { 
+        var mEnterIMG = "img/kurubaklava.jpg"; 
+          var mLeaveIMG ="img/kurubaklava1.jpg"; 
+                         
+           $( "#test5" ).hover(function(){ 
+                  $(this).attr("src", mEnterIMG); 
+                 }, function(){ 
+               $(this).attr("src", mLeaveIMG); 
+                            }); 
+                        }); 
+        
+     $(document).ready(function() { 
+              var mEnterIMG ="img/cevizli.jpg"; 
+                 var mLeaveIMG = "img/cevizli1.jpg"; 
+                             
+                $( "#test6" ).hover(function(){ 
+                 $(this).attr("src", mEnterIMG); 
+                  }, function(){ 
+                     $(this).attr("src", mLeaveIMG); 
+                                }); 
+                            }); 
+
+
+
+
+       $(document).ready(function() { 
+                 var mEnterIMG ="img/özelkare.jpg"; 
+                var mLeaveIMG = "img/tepsiözelkare.jpg"; 
+                                               
+                 $( "#test7" ).hover(function(){ 
+                  $(this).attr("src", mEnterIMG); 
+                        }, function(){ 
+                      $(this).attr("src", mLeaveIMG); 
+                                                  }); 
+                                              }); 
+
+
+       $(document).ready(function() { 
+                    var mEnterIMG ="img/baklava.jpg"; 
+                     var mLeaveIMG = "img/tepsibaklava.jpg"; 
+                                                               
+                     $( "#test8" ).hover(function(){ 
+                      $(this).attr("src", mEnterIMG); 
+                       }, function(){ 
+                      $(this).attr("src", mLeaveIMG); 
+                                }); 
+                           }); 
+
+             $(document).ready(function() { 
+                  var mEnterIMG ="img/midye.jpg"; 
+                   var mLeaveIMG = "img/tepsimidye.jpg"; 
+                                           
+                 $( "#test9" ).hover(function(){ 
+                     $(this).attr("src", mEnterIMG); 
+                     }, function(){ 
+                        $(this).attr("src", mLeaveIMG); 
+                                        }); 
+                                          }); 
+                   $(document).ready(function() { 
+                           var mEnterIMG ="img/kadayıf.jpg"; 
+                               var mLeaveIMG = "img/tepsikadayıf.jpg"; 
+                                                           
+                              $( "#test10" ).hover(function(){ 
+                               $(this).attr("src", mEnterIMG); 
+                               }, function(){ 
+                                 $(this).attr("src", mLeaveIMG); 
+                                                 }); 
+                                                        }); 
+                     $(document).ready(function() { 
+                              var mEnterIMG ="img/karısık.jpg"; 
+                                      var mLeaveIMG = "img/karısık1.jpg"; 
+                                                                           
+                                   $( "#test11" ).hover(function(){ 
+                                         $(this).attr("src", mEnterIMG); 
+                                          }, function(){ 
+                                        $(this).attr("src", mLeaveIMG); 
+                                                                  }); 
+                                                                          }); 
+        
